15e1219c2ffea0628ca749906769263570d144c9
pixelotes <donpastor@gmail.com>
2025-12-04 08:16:02 +0100

    chore: cambio en info_sistema
    No me terminaba de convencer la forma en la que saco la variable PRETTY_NAME, prefiero hacerlo en un one-liner porque así va un poco más en línea con lo que he dado en clase y muestro el manejo de más herramientas

---
+++ Raul_Pastor_Clemente/Apartado_2/info_sistema.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Obtener el usuario actual
+USUARIO=$(whoami)
+
+# Obtener el nombre del equipo (hostname)
+EQUIPO=$(hostname)
+
+# Obtener el Sistema Operativo
+if [ -f /etc/os-release ]; then
+    # Extraemos la variable PRETTY_NAME del archivo os_release
+    SO=$(grep '^PRETTY_NAME=' /etc/os-release | cut -d= -f2- | tr -d '"')
+else
+    # Fallback por si el sistema es muy antiguo
+    SO=$(uname -sr)
+fi
+
+# Obtener la dirección MAC
+# Snippet obtenido en https://stackoverflow.com/questions/23828413/get-mac-address-using-shell-script
+MAC=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address)
+
+# Si no encuentra MAC mostramos un mensaje
+if [ -z "$MAC" ]; then
+    MAC="No detectada"
+fi
+
+# Imprimir el mensaje con todos los datos
+echo "------------------------------------------------------------------"
+echo "Hola $USUARIO. Estás operando en el equipo '$EQUIPO'"
+echo "Sistema Operativo detectado: $SO"
+echo "Dirección MAC principal: $MAC"
+echo "------------------------------------------------------------------"
